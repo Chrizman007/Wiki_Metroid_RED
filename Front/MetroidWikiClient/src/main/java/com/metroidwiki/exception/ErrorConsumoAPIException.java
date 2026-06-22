8370adb7ae005b861e0e6f5a67df3081cd02ffdf
Mellow <pastelmarsh96@gmail.com>
2026-06-21 18:20:12 -0600

    Updated ErrorConsumoAPIException & RetrofitClient.java
    chore: ErrorConsumoAPIException ahora está preparado para manejar errores del backend que manden nombre para poder diferenciarlos

--- Front/MetroidWikiClient/src/main/java/com/metroidwiki/exception/ErrorConsumoAPIException.java
+++ Front/MetroidWikiClient/src/main/java/com/metroidwiki/exception/ErrorConsumoAPIException.java
@@ -2,13 +2,19 @@ package com.metroidwiki.exception;
 
 public class ErrorConsumoAPIException extends Exception {
     private final int codigoRespuesta;
+    private final String errorBackend;
 
-    public ErrorConsumoAPIException(String mensaje, int codigoRespuesta) {
+    public ErrorConsumoAPIException(String mensaje, int codigoRespuesta, String errorBackend) {
         super(mensaje);
         this.codigoRespuesta = codigoRespuesta;
+        this.errorBackend = errorBackend;
     }
 
     public int getCodigoRespuesta() {
         return codigoRespuesta;
+    }
+
+    public String getErrorBackend() {
+        return errorBackend;
     }
 }
--- Front/MetroidWikiClient/src/main/java/com/metroidwiki/network/RetrofitClient.java
+++ Front/MetroidWikiClient/src/main/java/com/metroidwiki/network/RetrofitClient.java
@@ -1,7 +1,14 @@
 package com.metroidwiki.network;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.metroidwiki.exception.ErrorConsumoAPIException;
+import okhttp3.OkHttpClient;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class RetrofitClient {
     public static final String BASE_URL = "http://18.224.252.1:3000";
@@ -9,9 +16,16 @@ public class RetrofitClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+                    .build();
+
             try {
                 retrofit = new Retrofit.Builder()
                         .baseUrl(BASE_URL)
+                        .client(okHttpClient)
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
             } catch (IllegalArgumentException e) {
@@ -20,5 +34,32 @@ public class RetrofitClient {
             }
         }
         return retrofit;
+    }
+
+    public static <T> void procesarRespuestaError(Response<T> response) throws ErrorConsumoAPIException {
+        int codigo = response.code();
+        String errorBackend = "ErrorDesconocido";
+        String mensaje = "Ocurrió un fallo al comunicarse con el servidor.";
+
+        if (response.errorBody() != null) {
+            try {
+                String cuerpoError = response.errorBody().string();
+                
+                JsonObject jsonObject = new Gson().fromJson(cuerpoError, JsonObject.class);
+                
+                if (jsonObject != null) {
+                    if (jsonObject.has("error")) {
+                        errorBackend = jsonObject.get("error").getAsString();
+                    }
+                    if (jsonObject.has("message")) {
+                        mensaje = jsonObject.get("message").getAsString();
+                    }
+                }
+            } catch (Exception e) { 
+                mensaje = "Error al decodificar la respuesta del servidor: " + e.getMessage();
+            }
+        }
+
+        throw new ErrorConsumoAPIException(mensaje, codigo, errorBackend);
     }
 }
